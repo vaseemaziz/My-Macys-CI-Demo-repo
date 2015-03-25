59fb744354222783a094cfcc17f5ad0892df23bc
Vaseem <vaseem.aziz@outlook.com>
2015-03-26 01:24:24 +0530

    Updated Code2

--- Code2.java
+++ Code2.java
@@ -2,8 +2,8 @@ public class Code2
 {
    public static void main(String args[])
    {
-      System.out.println("Hello World from Code 2");
-      System.out.println("Hello World from Code 2");
-      System.out.println("Hello World from Code 2");
+      System.out.println("Hello World from Code 2 once ");
+      System.out.println("Hello World from Code 2 twice");
+      System.out.println("Hello World from Code 2 thrice");
    }
 }
